6ef3b02c59c48accd056d1e56c48c489c650bd4a
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-07-17 02:51:42 -0600

    fix property types

--- schemaTypes/property.ts
+++ schemaTypes/property.ts
@@ -1,38 +1,16 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export const property = defineType({
   name: 'property',
   title: 'Property',
   type: 'document',
   fields: [
-    defineField({
-      name: 'id',
-      type: 'string',
-    }),
-    defineField({
-      name: 'title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      type: 'string',
-    }),
-    defineField({
-      name: 'address',
-      type: 'string',
-    }),
-    defineField({
-      name: 'price',
-      type: 'number',
-    }),
-    defineField({
-      name: 'bedrooms',
-      type: 'number',
-    }),
-    defineField({
-      name: 'bathrooms',
-      type: 'number',
-    }),
+    defineField({ name: 'title', type: 'string' }),
+    defineField({ name: 'description', type: 'string' }),
+    defineField({ name: 'address', type: 'string' }),
+    defineField({ name: 'price', type: 'number' }),
+    defineField({ name: 'bedrooms', type: 'number' }),
+    defineField({ name: 'bathrooms', type: 'number' }),
     defineField({
       name: 'images',
       type: 'array',
@@ -43,23 +21,21 @@ export const property = defineType({
       type: 'reference',
       to: [{ type: 'agent' }],
     }),
-    defineField({
-      name: 'sqft',
-      type: 'number',
-    }),
-    defineField({
-      name: 'dateAdded',
-      type: 'string',
-    }),
+    defineField({ name: 'sqft', type: 'number' }),
+    defineField({ name: 'dateAdded', type: 'string' }),
     defineField({
       name: 'type',
+      title: 'Property Type',
       type: 'array',
-      of: [{ name: 'house', type: 'string' }, { name: 'apartment', type: 'string' }, { name: 'condo', type: 'string' }, { name: 'townhouse', type: 'string' }]
+      of: [{ type: 'string' }],
+      options: {
+        list: ['house', 'apartment', 'condo', 'townhouse'],
+      },
     }),
     defineField({
       name: 'features',
       type: 'array',
-      of: [{ type: 'string' }]
-    })
+      of: [{ type: 'string' }],
+    }),
   ],
 })
